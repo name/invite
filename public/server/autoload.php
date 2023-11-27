5618ea145e218ea255fe733fc78054048338137b
charlie <charlie@maddex.dev>
2023-11-27 23:35:42 +0000

    add autoload.php file with global variables

---
+++ public/server/autoload.php
@@ -0,0 +1,9 @@
+<?php
+
+$GLOBALS = [
+    'PROJECT_NAME' => 'Multiorb',
+    'REDIRECT_URL' => 'https://x.com/multiorb',
+    'PROJECT_INVITE_URL' => 'https://invite.multiorb.net',
+    'PROJECT_LOGO_URL' => 'https://multiorb.net/favicon.png',
+];
+require_once($_SERVER['DOCUMENT_ROOT'] . '/server/database.php');
